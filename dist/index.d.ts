aefe9fe38e893cad21df6cbe581d4d56c225f9de
Reece Como <reece.como@gmail.com>
2025-04-17 15:45:08 +1000

    wip

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -860,17 +860,17 @@ export type GamepadNamedBindEvent = {
 	axisCode: AxisCode;
 };
 export type HapticEffect = {
-	/** How long the vibration lasts. */
+	/** How long the vibration lasts (in milliseconds) */
 	duration: number;
-	/** Buzz - Strength of the high-frequency motor (feels more like a "buzz") */
+	/** Strength of the high-frequency motor (feels more like a "buzz") */
 	strongMagnitude?: number;
-	/** Rumble - Strength of the low-frequency motor (feels more like a "rumble") */
+	/** Strength of the low-frequency motor (feels more like a "rumble") */
 	weakMagnitude?: number;
 	/** Strength of the left trigger motor (if supported) */
 	leftTrigger?: number;
 	/** Strength of the right trigger motor (if supported) */
 	rightTrigger?: number;
-	/** Delays the start of the vibration effect. */
+	/** Delay the start of the vibration effect (in milliseconds) */
 	startDelay?: number;
 } & {
 	vibrationType?: "dual-rumble" | "trigger-rumble";
